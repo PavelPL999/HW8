ff6b53401db1ddeefa4f3a7338f85034b5e8c291
Pavel <paleon@mail.ru>
2024-03-30 22:30:02 +0300

    HW8

--- HW8/Task1/src/Main.java
+++ HW8/Task1/src/Main.java
@@ -1,15 +1,30 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         //#1 Создать массив из 10 целых чисел. Значения могут быть любыми
-        //todo
-
+        int[] numbers = new int[10];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = new Random().nextInt(100);
+        }
         //#2 Распечатать все значения массива начиная с 0 до последнего индексов.
-        //todo
-
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
+        System.out.println();
         //#3 Распечатать все значения массива с конца (начиная с последнего до 0 индексов).
-        //todo
-
+        for (int i = numbers.length - 1; i >= 0 ; i--) {
+            System.out.print(numbers[i] + " ");
+        }
+        System.out.println();
         //#4 *** Задание особой сложности. Развернуть массив(см конец урока).
-        //todo
+        for (int i = 0; i < numbers.length / 2; i++) {
+            int temp = numbers[i];
+            numbers[i] = numbers[numbers.length - 1 - i];
+            numbers[numbers.length - 1 - i] = temp;
+        }
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
     }
 }
--- HW8/Task2/src/Main.java
+++ HW8/Task2/src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        //todo код программы писать тут
+        double[] weights = new double[10];
+        double averageWeight = 0;
+        int countPeople = 0;
+        for (int i = 0; i < weights.length; i++) {
+            weights[i] = (new Random().nextDouble() * 60) + 40;
+            averageWeight += weights[i];
+            if (weights[i] >= 60 && weights[i] <= 80) {
+                countPeople++;
+            }
+        }
+        System.out.println("Средний вес всех людей: " + averageWeight / weights.length + " кг");
+        System.out.println("Кол-во людей, у к/х вес в промежутке от 60 до 80 кг включительно: " + countPeople);
     }
 }
